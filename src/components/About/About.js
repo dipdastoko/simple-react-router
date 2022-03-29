753d7e6ff2f713012cac93d7dfc51fb3aff56737
dipdastoko <dipdastoko@gmail.com>
2022-03-29 18:01:40 +0600

    load data

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>This is about our page</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vero labore veniam expedita doloribus quasi impedit hic. Quae suscipit repellat deserunt eos repudiandae voluptas. Animi amet aspernatur architecto quidem suscipit.</p>
+        </div>
+    );
+};
+
+export default About;
